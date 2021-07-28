f7901b5c5f79db83569d9aad49283230b7c43080
Muhammad Zain Tariq <49867382+mzaintariq@users.noreply.github.com>
2021-07-28 17:03:43 +0500

    Separated action files

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
 import { GET_TOKEN, getToken, setToken } from './authActions'
+import { GET_USER_DATA, getUserData, setUserData } from './userActions'
 
 export { GET_TOKEN, getToken, setToken };
+export { GET_USER_DATA, getUserData, setUserData };
