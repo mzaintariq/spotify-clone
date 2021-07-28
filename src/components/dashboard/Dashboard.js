04cdc047c86f27b1aad94e248227daf28d6a0351
Muhammad Zain Tariq <49867382+mzaintariq@users.noreply.github.com>
2021-07-28 16:51:58 +0500

    Removed extra code

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -13,17 +13,12 @@ import Library from '../library/Library'
 
 
 function Dashboard() {
-  // const myState = useSelector((state) => state.userReducer);
   const myState = useSelector((state) => state.authReducer);
-  const myState2 = useSelector((state) => state.userReducer);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getUserData(myState.accessToken));
   }, [])
-
-  console.log("CHECK: ", myState)
-  console.log("CHECK 2: ", myState2)
 
   return (
     <>
@@ -43,24 +38,10 @@ function Dashboard() {
             <Route path="/">
               <Home />
             </Route>
-            {/* <Route path="/">
-              <Home />
-            </Route> */}
           </Switch>
         </div>
-        
         <Footer />
       </Router>
-    
-      {/* <div className="dashboard">
-        <h1>Logged In!</h1>
-        <h1>Access Token:</h1>
-        <p>{myState.accessToken}</p>
-        <h1>Refresh Token:</h1>
-        <p>{myState.refreshToken}</p>
-        <h1>Expires In:</h1>
-        <p>{myState.expiresIn}</p>
-      </div> */}
     </>
   );
 }
