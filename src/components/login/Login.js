2d2b7366be6dd528e0050339dc046ab03ede70ae
Muhammad Zain Tariq <49867382+mzaintariq@users.noreply.github.com>
2021-07-28 16:37:33 +0500

    Added logo asset

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Login.scss'
-import { loginUrl } from "../../app/spotify"
+import { loginUrl } from '../../app/spotify'
+import SpotifyLogo from '../../assets/spotifylogo.jpeg'
 
 function Login() {
     return (
         <div className='login'>
-            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
+            {/* <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" /> */}
+            <img src={SpotifyLogo} alt="" />
             <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
         </div>
     )
