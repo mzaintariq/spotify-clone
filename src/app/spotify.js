16eeb88602759d9ec67c0cec3ac0f64e83af668e
Muhammad Zain Tariq <49867382+mzaintariq@users.noreply.github.com>
2021-07-30 17:37:04 +0500

    react-spotify-web-playback

--- src/app/spotify.js
+++ src/app/spotify.js
@@ -5,6 +5,9 @@ const scopes = [
     "user-read-playback-state",
     "user-top-read",
     "user-modify-playback-state",
+    "streaming",
+    "user-read-email",
+    "user-read-private"
 ]
 export const redirectUri = "http://localhost:3000/";
 export const clientId = "9c2b9616464949a9a3e68c21250663a0";
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import './Footer.scss'
 import { Link } from "react-router-dom";
+import SpotifyPlayer from 'react-spotify-web-playback';
+import { useSelector, useDispatch } from 'react-redux';
 
 
 function Footer() {
+  const myState = useSelector((state) => state.authReducer);
+  const myState2 = useSelector((state) => state.playlistReducer);
+  const dispatch = useDispatch();
+
+  console.log("CHECK PLAYER TOKEN: ", myState.accessToken)
+  // console.log("CHECK PLAYER TOKEN 2: ", myState2.playlistData.tracks.items[0].track.uri)
+
   return (
     <div className="footer">
-      <h2>Footer</h2>
+      {/* <h2>Footer</h2> */}
       {/* Footer */}
+      <SpotifyPlayer
+        token={myState.accessToken}
+        uris={['spotify:track:0MWiSBKm8Avs8iDIxcertp', 'myState2.playlistData.tracks.items[1].track.uri']}
+        styles={{
+          activeColor: '#fff',
+          bgColor: '#181818',
+          color: '#fff',
+          loaderColor: '#fff',
+          sliderColor: '#1cb954',
+          trackArtistColor: '#ccc',
+          trackNameColor: '#fff',
+        }}
+      />
     </div>
   )
 }
