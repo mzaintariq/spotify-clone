8a8c25d777cf2d9299f474d30d47582bce6da1ea
Muhammad Zain Tariq <49867382+mzaintariq@users.noreply.github.com>
2021-08-02 12:13:41 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.scss";
 import React, { useEffect } from "react";
-import Dashboard from "./components/dashboard/Dashboard";
 import Login from "./components/login/Login";
 import { useSelector, useDispatch } from "react-redux";
 import { getToken } from "./actions/index";
@@ -9,15 +8,13 @@ function App() {
   const myState = useSelector((state) => state.authReducer);
   const dispatch = useDispatch();
 
-  useEffect(async () => {
+  useEffect(() => {
     const code = new URLSearchParams(window.location.search).get("code");
     if (code) {
       dispatch(getToken(code));
       window.history.pushState("", "", "/");
     }
   }, []);
-
-  console.log("CHECK FINAL: ", myState.accessToken);
 
   return (
     <div className="app">
