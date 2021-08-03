d0829c5f9732199f80cc05e5792a503d4cc6d140
Muhammad Zain Tariq <49867382+mzaintariq@users.noreply.github.com>
2021-08-03 17:45:07 +0500

    SongRow

--- src/components/songrow/SongRow.js
+++ src/components/songrow/SongRow.js
@@ -1,34 +1,41 @@
-import React from 'react'
-import './SongRow.scss'
-import ExplicitIcon from '@material-ui/icons/Explicit';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import React from "react";
+import "./SongRow.scss";
+import ExplicitIcon from "@material-ui/icons/Explicit";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 
 function SongRow({ track, id }) {
-    return (
-        <div className="songrow">
-            <div className="songrow__number">
-                <h1>{id+1}</h1> 
-            </div>
-            <img className="songrow__album" src={track.album.images[0].url} alt=""/>
-            <div className="songrow__info">
-                <h1>{track.name}</h1>
-                {track.explicit ? 
-                    <div className="songrow__name">
-                        <ExplicitIcon className="explicit__icon" />
-                        <p>
-                            {track.artists.map(artist => artist.name).join(", ")} - { }
-                            {track.album.name}
-                        </p>
-                    </div>
-                    :
-                    <p>
-                        {track.artists.map(artist => artist.name).join(", ")} - { }
-                        {track.album.name}
-                    </p>
-                }
-            </div>
+  return (
+    <div className="songrow">
+      <div className="songrow__number">
+        <h4>{id + 1}</h4>
+      </div>
+      <div className="songrow__albumart">
+        <img
+          className="songrow__album"
+          src={track.album.images[0].url}
+          alt=""
+        />
+      </div>
+      <div className="songrow__small">
+        <div className="songrow__info">
+          <div className="songrow__info__name">
+            <h1>{track.name}</h1>
+          </div>
+          <div className="songrow__info__artist">
+            <p>
+              {track.artists.map((artist) => artist.name).join(", ")} - {}
+              {track.album.name}
+            </p>
+          </div>
         </div>
-    )
+        {track.explicit ? (
+          <ExplicitIcon className="songrow__info__icon" />
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default SongRow
+export default SongRow;
