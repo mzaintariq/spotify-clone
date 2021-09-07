19b957eab94bfbcbcd4b77d70fa2534ace52b17a
Muhammad Zain Tariq <49867382+mzaintariq@users.noreply.github.com>
2021-09-07 14:00:36 +0500

    update store

--- src/store.js
+++ src/store.js
@@ -1,46 +1,3 @@
-// import { createStore, applyMiddleware } from "redux";
-// import createSagaMiddleware from "redux-saga";
-// import { persistStore, persistReducer } from 'redux-persist'
-// import storage from 'redux-persist/lib/storage'
-
-// import rootReducer from "./reducers/index";
-// import { watcherSaga } from "./sagas/rootSaga";
-
-// // const sagaMiddleware = createSagaMiddleware();
-// // const middleware =
-// //   process.env.NODE_ENV !== "production"
-// //     ? [require("redux-immutable-state-invariant").default(), sagaMiddleware]
-// //     : [sagaMiddleware];
-
-// // const store = createStore(rootReducer, {}, applyMiddleware(...middleware));
-
-// // sagaMiddleware.run(watcherSaga);
-
-// // export default store;
-
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-// }
-// const sagaMiddleware = createSagaMiddleware();
-// const middleware =
-//   process.env.NODE_ENV === "development"
-//     ? [require("redux-immutable-state-invariant").default(), sagaMiddleware]
-//     : [sagaMiddleware];
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-// // const store = createStore(rootReducer, {}, applyMiddleware(...middleware));
-
-// const store = createStore(persistedReducer, {}, applyMiddleware(...middleware))
-// export const persistor = persistStore(store)
-
-// sagaMiddleware.run(watcherSaga);
-
-// // export default store;
-// // export default store;
-// export default store;
-
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { persistStore, persistReducer } from "redux-persist";
@@ -63,7 +20,6 @@ const middleware =
     : [sagaMiddleware];
 
 export const store = createStore(persistedReducer, {}, applyMiddleware(...middleware));
-// export const store = createStore(rootReducer, {}, applyMiddleware(...middleware));
 export const persistor = persistStore(store);
 
 sagaMiddleware.run(watcherSaga);
