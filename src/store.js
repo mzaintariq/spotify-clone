9652161dae63a63e667a400b909c2048d36bec0e
Muhammad Zain Tariq <49867382+mzaintariq@users.noreply.github.com>
2021-09-07 13:17:30 +0500

    update

--- src/store.js
+++ src/store.js
@@ -1,16 +1,53 @@
 import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./reducers/index";
 import createSagaMiddleware from "redux-saga";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+import rootReducer from "./reducers/index";
 import { watcherSaga } from "./sagas/rootSaga";
 
+// const sagaMiddleware = createSagaMiddleware();
+// const middleware =
+//   process.env.NODE_ENV !== "production"
+//     ? [require("redux-immutable-state-invariant").default(), sagaMiddleware]
+//     : [sagaMiddleware];
+
+// const store = createStore(rootReducer, {}, applyMiddleware(...middleware));
+
+// sagaMiddleware.run(watcherSaga);
+
+// export default store;
+
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 const sagaMiddleware = createSagaMiddleware();
 const middleware =
   process.env.NODE_ENV !== "production"
     ? [require("redux-immutable-state-invariant").default(), sagaMiddleware]
     : [sagaMiddleware];
 
-const store = createStore(rootReducer, {}, applyMiddleware(...middleware));
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// const store = createStore(rootReducer, {}, applyMiddleware(...middleware));
+
+const store = createStore(persistedReducer, {}, applyMiddleware(...middleware))
+// const persistor = persistStore(store)
 
 sagaMiddleware.run(watcherSaga);
 
+// export default store;
 export default store;
+
+
+
+
+ 
+ 
+// export default () => {
+//   let store = createStore(persistedReducer)
+//   let persistor = persistStore(store)
+//   return { store, persistor }
+// }
